6b766e913231741856e27b5662115398a31e0ef7
Chris Kingdon <ckgiveout@gmail.com>
2017-10-10 15:10:12 -0400

    Working on writeln driver

--- kmain.c
+++ kmain.c
@@ -1,3 +1,14 @@
+#include "io.h"
+
+/* The I/O ports */
+#define FB_COMMAND_PORT   0x3D4
+#define FB_DATA_PORT      0x3D5
+
+/* The I/O port commands */
+#define FB_HIGH_BYTE_COMMAND 14
+#define FB_LOW_BYTE_COMMAND  15
+
+/* The frame buffer colors */
 #define FB_BLACK          0
 #define FB_BLUE           1
 #define FB_GREEN          2
@@ -17,6 +28,18 @@
 
 /* The framebuffer for writing to the screen */
 char *fb = (char *) 0x000B8000;
+unsigned short cursorPos = 0;
+/** fb_move_cursor:
+ * Moves the cursor of the framebuffer to the given position
+ *
+ * @param pos The new position of the cursor
+ */
+void fb_move_cursor(unsigned short pos) {
+  outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+  outb(FB_DATA_PORT, ((pos >> 8) & 0x00FF));
+  outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+  outb(FB_DATA_PORT, pos & 0x00FF);
+}
 
 /** fb_write_cell:
  * Writes a character with the given foreground and background to position i
@@ -32,20 +55,28 @@ void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned bg) {
   fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
 }
 
+int writeln(char *buf, unsigned int len) {
+  unsigned int x = 0, i = 0;
+
+  // print the message to the framebuffer
+  for(; i < (2 * len); i+=2) {
+    //If the character is a newline go down a line
+    if(buf[x] == '\n') {
+      fb_move_cursor();
+    } else {
+      fb_write_cell(i, buf[x], FB_BLACK, FB_GREEN);
+      cursorPos++;
+    }
+    x++;
+  }
+  return 0;
+}
+
 /* The main program loop */
 int main() {
-  fb_write_cell(0, 'H', FB_BLACK, FB_GREEN);
-  fb_write_cell(2, 'E', FB_BLACK, FB_GREEN);
-  fb_write_cell(4, 'L', FB_BLACK, FB_GREEN);
-  fb_write_cell(6, 'L', FB_BLACK, FB_GREEN);
-  fb_write_cell(8, 'O', FB_BLACK, FB_GREEN);
-  fb_write_cell(10, ' ', FB_BLACK, FB_GREEN);
-  fb_write_cell(12, 'W', FB_BLACK, FB_GREEN);
-  fb_write_cell(14, 'O', FB_BLACK, FB_GREEN);
-  fb_write_cell(16, 'R', FB_BLACK, FB_GREEN);
-  fb_write_cell(18, 'L', FB_BLACK, FB_GREEN);
-  fb_write_cell(20, 'D', FB_BLACK, FB_GREEN);
-  fb_write_cell(22, '!', FB_BLACK, FB_GREEN);
+  char message[24] = "Hello World!\n more text";
+  writeln(message, 24);
+  writeln(message, 24);
 
   return 0;
 }
